1ff6828a9a2cb67a42f32f3139d5b4390a2a6d61
Josh Vinluan <153298299+JoshVinluan@users.noreply.github.com>
2025-04-15 22:08:22 +0800

    Added serial commands for servo motor functions

--- SerialCommsPkg/SerialCommsPkg/serialInitialize.py
+++ SerialCommsPkg/SerialCommsPkg/serialInitialize.py
@@ -2,24 +2,30 @@ import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Bool
 from std_msgs.msg import Int32
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PointStamped, Twist
 import serial
 import threading
 
 class SerialInitializeNode(Node):
     def __init__(self):
         super().__init__('serial_initialize')
-        self.serial_port = '/dev/ttyUSB1' 
-        self.baudrate = 115200
 
-        #### Initialize the serial connection ####
-        try:
-            self.serial_connection = serial.Serial(self.serial_port, self.baudrate, timeout=1)
-            self.get_logger().info(f'Successfully connected to {self.serial_port} at {self.baudrate} baud.')
-        except serial.SerialException as e:
-            self.get_logger().error(f'Failed to connect to {self.serial_port}: {e}')
+        possible_ports = ['/dev/ttyUSB0', '/dev/ttyUSB1']
+        self.baudrate = 115200
+        self.serial_connection = None
+
+        for port in possible_ports:
+            try:
+                self.serial_connection = serial.Serial(port, self.baudrate, timeout=1)
+                self.serial_port = port
+                self.get_logger().info(f'Successfully connected to {self.serial_port} at {self.baudrate} baud.')
+                break
+            except serial.SerialException as e:
+                self.get_logger().warn(f'Failed to connect to {port}: {e}')
+        
+        if self.serial_connection is None:
+            self.get_logger().error('Failed to connect to any serial port.')
             return
-        #### END OF: Initialize the serial connection ####
 
         #### Subscriber to "toClean" topic ####
         self.toClean_subscriber = self.create_subscription(
@@ -30,6 +36,15 @@ class SerialInitializeNode(Node):
         )
         self.get_logger().info('Subscriber to "toClean" topic initialized.')
         #### END OF: Subscriber to "toClean" topic ####
+
+        self.cmd_vel_stamped_subscriber = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.cmd_vel_callback,
+            10
+        )
+        self.get_logger().info('Subscriber to "cmd_vel" topic initialized.')
+        #### END OF: Subscriber to "cmd_vel_stamped" topic ####
 
         #### Publishers for sensor readings ####
         self.tof_publisher = self.create_publisher(
@@ -45,7 +60,7 @@ class SerialInitializeNode(Node):
         #### END OF: Publishers for sensor readings ####
 
         #### Start a thread to continuously read from the serial port ####
-        self.read_thread = threading.Thread(target=self.read_from_serial)
+        self.read_thread = threading.Thread(target=self.tofScan)
         self.read_thread.daemon = True
         self.read_thread.start()
         #### END OF: Start a thread to continuously read from the serial port ####
@@ -63,7 +78,19 @@ class SerialInitializeNode(Node):
         except Exception as e:
             self.get_logger().error(f"Error writing to serial port: {e}")
     
-    def read_from_serial(self):
+    def cmd_vel_callback(self, msg: Twist):
+        # Send velocity commands to ESP32 via serial
+        try:
+            if self.serial_connection.is_open:
+                message = f"vlin:{msg.linear.x},vang:{msg.angular.z}\n"
+                self.serial_connection.write(message.encode())
+                self.get_logger().info(f"Sent to ESP32: {message.strip()}")
+            else:
+                self.get_logger().error("Unable to send velocity comamnds. Serial connection is not open.")
+        except Exception as e:
+            self.get_logger().error(f"Error writing to serial port: {e}")
+    
+    def tofScan(self):
         # Continuously read from the serial port
         while rclpy.ok():
             try:
